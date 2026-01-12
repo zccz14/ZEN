19cfd1765cc102d582120adb24632fdb2903a30b
CZ <cz@ntnl.io>
2026-01-12 21:13:19 +0800

    feat: update version to 0.3.3, enhance ContentPage component with related content display and non-critical CSS loading, and add global styles for html and body

--- src/ssg/style.ts
+++ src/ssg/style.ts
@@ -1,5 +1,12 @@
 export const style = `
 
+      html, body {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100%;
+      }
+
       html[lang='ar-SA'] {
         direction: rtl;
       }
